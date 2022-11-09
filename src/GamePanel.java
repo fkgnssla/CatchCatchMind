2b4831f1012aa0c2851a720ea56247aa243a2dd0
cyujin <113533845+cyujin@users.noreply.github.com>
2022-11-09 11:09:12 +0900

    배경사진 변경

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -16,7 +16,7 @@ import javax.swing.JTextField;
 
 public class GamePanel extends JPanel{
 	
-	private Image gamebackground = new ImageIcon("src/image/backimage.png").getImage();
+	private Image gamebackground = new ImageIcon("src/image/backimg.jpg").getImage();
 	private ImageIcon userImg = new ImageIcon("src/image/userimage.png");
 	private ImageIcon userImg2 = new ImageIcon("src/image/userimage2.png");
 	private ImageIcon canvasImg = new ImageIcon("src/image/canvas.png");
@@ -25,6 +25,7 @@ public class GamePanel extends JPanel{
 	
 	private JPanel contentPane;
 	private JPanel colorBar;
+	private JPanel userPanel;
 	private JTextField textField;
 	private JTextField username;
 	private JTextField username2;
@@ -60,7 +61,7 @@ public class GamePanel extends JPanel{
 		
 		//------그림 화면
 		contentPane = new JPanel();
-		contentPane.setBounds(200, 120, 450, 300);
+		contentPane.setBounds(208, 136, 431, 314);
 		contentPane.setForeground(new Color(0, 0, 0));
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setLayout(null);
@@ -68,20 +69,20 @@ public class GamePanel extends JPanel{
 		
 		JLabel canvasLabel = new JLabel(canvasImg);
 		canvasLabel.setBounds(200, 100, 450, 30);
-		add(canvasLabel);
+		//add(canvasLabel);
 		
 		//------그림 화면 끝
 		
 		//------제시어 화면
 		JLabel wordLabel = new JLabel("제시어");
 		wordLabel.setFont(new Font("맑은 고딕", Font.BOLD, 16));
-		wordLabel.setBounds(480, 60, 400, 40);
+		wordLabel.setBounds(480, 75, 208, 40);
 		add(wordLabel);
 		
 		showWord = new JTextField();
 		showWord.setFont(new Font("맑은 고딕", Font.BOLD, 12));
 		showWord.setText("딸기맛 사탕");
-		showWord.setBounds(535, 66, 100, 25);
+		showWord.setBounds(535, 81, 100, 25);
 		showWord.setEnabled(false);
 		showWord.setColumns(10);
 		add(showWord);
@@ -108,64 +109,88 @@ public class GamePanel extends JPanel{
 		//------게임 채팅 끝
 		
 		//------사용자
+		//user1
 		JLabel user1Label = new JLabel(userImg);
-		user1Label.setBounds(20, 40, 50, 50);
+		user1Label.setBounds(106, 112, 68, 110);
 		add(user1Label);
 		username = new JTextField();
-		username.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		username.setFont(new Font("맑은 고딕", Font.BOLD, 11));
 		username.setText("user1");
-		username.setBounds(20, 60, 100, 20);
+		username.setBounds(37, 129, 68, 22);
 		username.setEnabled(false);
 		username.setColumns(10);
 		add(username);
 		
 		JLabel score1Label = new JLabel("SCORE");
 		score1Label.setFont(new Font("맑은 고딕", Font.PLAIN, 10));
-		score1Label.setBounds(10,103,80,20);
+		score1Label.setBounds(39,184,65,17);
 		add(score1Label);
 		JLabel score1Label_score = new JLabel("50");
-		score1Label_score.setFont(new Font("맑은 고딕", Font.PLAIN, 10));
-		score1Label_score.setBounds(90, 125, 80, 20);
+		score1Label_score.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
+		score1Label_score.setBounds(40, 160, 65, 17);
 		add(score1Label_score);
 		
+		//user2
+		JLabel user2Label = new JLabel(userImg);
+		user2Label.setBounds(105, 214, 68, 110);
+		add(user2Label);
+		username = new JTextField();
+		username.setFont(new Font("맑은 고딕", Font.BOLD, 11));
+		username.setText("user2");
+		username.setBounds(36, 231, 68, 22);
+		username.setEnabled(false);
+		username.setColumns(10);
+		add(username);
+		
+		JLabel score2Label = new JLabel("SCORE");
+		score2Label.setFont(new Font("맑은 고딕", Font.PLAIN, 10));
+		score2Label.setBounds(39,290,65,17);
+		add(score2Label);
+		JLabel score2Label_score = new JLabel("70");
+		score2Label_score.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
+		score2Label_score.setBounds(40, 260, 65, 17);
+		add(score2Label_score);
+		
+		//user3
 		JLabel user3Label = new JLabel(userImg);
-		user3Label.setBounds(670, 70, 50, 60);
+		user3Label.setBounds(746, 108, 68, 110);
 		add(user3Label);
 		username3 = new JTextField();
 		username3.setFont(new Font("맑은 고딕", Font.BOLD, 12));
 		username3.setText("user3");
-		username3.setBounds(720, 70, 80, 20);
+		username3.setBounds(676, 125, 68, 22);
 		username3.setEnabled(false);
 		username3.setColumns(10);
 		add(username3);
 		
 		JLabel score3Label = new JLabel("SCORE");
 		score3Label.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
-		score3Label.setBounds(725,100,70,20);
+		score3Label.setBounds(679, 180, 65, 17);
 		add(score3Label);
 		JLabel score3Label_score = new JLabel("50");
 		score3Label_score.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
-		score3Label_score.setBounds(775, 100, 70, 20);
+		score3Label_score.setBounds(679, 158, 65, 17);
 		add(score3Label_score);
 		
+		//user4
 		JLabel user4Label = new JLabel(userImg2);
-		user4Label.setBounds(670, 180, 50, 60);
+		user4Label.setBounds(744, 210, 68, 110);
 		add(user4Label);
 		username4 = new JTextField();
 		username4.setFont(new Font("맑은 고딕", Font.BOLD, 12));
 		username4.setText("user4");
-		username4.setBounds(720, 180, 80, 20);
+		username4.setBounds(674, 228, 68, 22);
 		username4.setEnabled(false);
 		username4.setColumns(10);
 		add(username4);
 		
 		JLabel score4Label = new JLabel("SCORE");
 		score4Label.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
-		score4Label.setBounds(725,210,70,20);
+		score4Label.setBounds(679, 280, 65, 17);
 		add(score4Label);
 		JLabel score4Label_score = new JLabel("50");
 		score4Label_score.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
-		score4Label_score.setBounds(775, 210, 70, 20);
+		score4Label_score.setBounds(679, 255, 65, 17);
 		add(score4Label_score);	
 		//------사용자 끝
 		
@@ -196,12 +221,12 @@ public class GamePanel extends JPanel{
 		btnBlue.setFocusPainted(false);btnGreen.setFocusPainted(false);
 		btnYellow.setFocusPainted(false);btnEraser.setFocusPainted(false);
 			
-		btnBlack.setBounds(250, 430, 42, 40);
-		btnRed.setBounds(300, 430, 42, 40);
-		btnBlue.setBounds(350, 430, 42, 40);
-		btnGreen.setBounds(400, 430, 42, 40);
-		btnYellow.setBounds(450, 430, 42, 40);
-		btnEraser.setBounds(500, 430, 42, 40);
+		btnBlack.setBounds(230, 460, 50, 50);
+		btnRed.setBounds(280, 460, 50, 50);
+		btnBlue.setBounds(330, 460, 50, 50);
+		btnGreen.setBounds(380, 460, 50, 50);
+		btnYellow.setBounds(430, 460, 50, 50);
+		btnEraser.setBounds(480, 460, 50, 50);
 		add(btnBlack); add(btnRed); add(btnBlue); add(btnGreen); add(btnYellow);add(btnEraser);
 		/*
 		colorBar.add(btnBlack);
